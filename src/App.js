d3dc505c2da8956acfbc8a12f3a73391266de741
Cychlope <m.marcelinus@gmail.com>
2022-11-15 21:48:47 +0700

    add Social Login Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 
 import SignIn from './pages/Auth/SignIn';
+import SignInSocial from './pages/Auth/SignInSocial';
 import SignUp from './pages/Auth/SignUp';
 import Register from './pages/Auth/Register';
 
@@ -10,6 +11,7 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Route component={SignIn} path="/sign-in" />
+        <Route component={SignInSocial} path="/sign-in-social" />
         <Route component={SignUp} path="/sign-up" />
         <Route component={Register} path="/register" />
       </BrowserRouter>
